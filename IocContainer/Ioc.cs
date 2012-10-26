5f2ab003236518ae4963c155fc2d962a6826c0d3
Alan Stump <alstump@umail.iu.edu>
2012-10-25 20:27:57 -0400

    initial commit

---
+++ IocContainer/Ioc.cs
@@ -0,0 +1,19 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocContainer
+{
+    public static class Ioc
+    {
+        static IKernel _standardKernel;
+
+        public Ioc()
+        {
+            _standardKernel = new StandardKernel();
+        }
+    }
+}
